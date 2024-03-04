eca1441133773ef08c20844dfc76d9ab38b48485
Shubhranshu-Pattanaik-Maersk <138875203+Shubhranshu-Pattanaik-Maersk@users.noreply.github.com>
2024-03-04 17:39:03 +0530

    Update release.py

--- .github/scripts/update_release/release.py
+++ .github/scripts/update_release/release.py
@@ -24,7 +24,7 @@ def create_release_task(GITHUB_REPO: str, OWNER: str, TAG_NAME: str, TARGET_COMM
     data = {"tag_name":TAG_NAME,"target_commitish":TARGET_COMMITISH,"name":NAME_RELEASE,"body":BODY,"draft":DRAFT,"prerelease":PRERELEASE,"generate_release_notes":GENERATE_NOTES}
 
     # try:
-        response = requests.post(f"https://api.github.com/repos/{OWNER}/workflow_test/releases", headers=headers, data=json.dumps(data))
+    response = requests.post(f"https://api.github.com/repos/{OWNER}/workflow_test/releases", headers=headers, data=json.dumps(data))
     #     response.raise_for_status()
 
     #     if response.status_code == 201:
