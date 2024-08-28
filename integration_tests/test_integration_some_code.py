6e5116fc77f1bdb2a72ab6518b862a5c386ecd1d
shubhranshu-pattanaik <shubhranshu.pattanaik@maersk.com>
2024-08-28 08:50:19 +0530

    done3

---
+++ integration_tests/test_integration_some_code.py
@@ -0,0 +1,35 @@
+# integration_tests/test_integration_some_code.py
+
+import unittest
+import psycopg2
+
+class TestIntegrationWithDatabase(unittest.TestCase):
+    
+    def setUp(self):
+        self.connection = psycopg2.connect(
+            dbname="test_db",
+            user="postgres",
+            password="postgres",
+            host="localhost",
+            port="5432"
+        )
+        self.cursor = self.connection.cursor()
+
+    def tearDown(self):
+        self.cursor.close()
+        self.connection.close()
+
+    def test_database_insertion(self):
+        # Example of an integration test involving the database
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS test_table (id serial PRIMARY KEY, name VARCHAR(50));")
+        self.cursor.execute("INSERT INTO test_table (name) VALUES ('Test Name');")
+        self.connection.commit()
+
+        self.cursor.execute("SELECT * FROM test_table WHERE name = 'Test Name';")
+        result = self.cursor.fetchone()
+
+        self.assertIsNotNone(result)
+        self.assertEqual(result[1], 'Test Name')
+
+if __name__ == '__main__':
+    unittest.main()
